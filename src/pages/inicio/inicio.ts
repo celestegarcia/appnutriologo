8bc423780f4bedd8cdc58c1b74f6f19b3fb1d328
celestegarcia <celeste_rut@hotmail.com>
2017-11-12 01:01:32 -0700

    agregar graficas

--- src/pages/inicio/inicio.ts
+++ src/pages/inicio/inicio.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'page-inicio',
   templateUrl: 'inicio.html'
 })
 export class InicioPage {
+  @ViewChild('barCanvas') barCanvas;
+  @ViewChild('lineCanvas') lineCanvas;
+
+  barChart: any;
+  lineChart: any;
 
   public user:any = {
     email:"",
@@ -27,7 +33,161 @@ export class InicioPage {
       */
     this.user=tmpUsr;    // navParams; // <- No funciona esta basura
     console.log(this.user);
-    
+
+       
   }
+
+  ionViewDidLoad() {
+    
+           this.barChart = new Chart(this.barCanvas.nativeElement, {
+    
+               type: 'bar',
+               data: {
+                   labels: ["Sesion Anterior", "Sesion Actual"],
+                   datasets: [{
+                       label: 'Peso',
+                       data: [12, 19],
+                       backgroundColor: [
+                           
+                           'rgba(54, 162, 235, 0.2)',
+                           'rgba(54, 162, 235, 0.2)',
+                           'rgba(54, 162, 235, 0.2)'
+                           
+                       ],
+                       borderColor: [
+                           
+                           'rgba(54, 162, 235, 1)',
+                           'rgba(54, 162, 235, 1)',
+                           'rgba(54, 162, 235, 1)'
+                           
+                       ],
+                       borderWidth: 1
+                   },
+                   {
+                    label: 'Masa Corporal',
+                    data: [1, 1],
+                    backgroundColor: [
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(75, 192, 192, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(75, 192, 192, 1)'
+                        
+                    ],
+                    borderWidth: 1
+                }, {
+                  label: '% Muscular',
+                  data: [12, 30],
+                  backgroundColor: [
+                      
+                      'rgba(255, 159, 64, 0.2)',
+                      'rgba(255, 159, 64, 0.2)',
+                      'rgba(255, 159, 64, 0.2)',
+                  ],
+                  borderColor: [
+                      
+                      'rgba(255, 159, 64, 1)',
+                      'rgba(255, 159, 64, 1)',
+                      'rgba(255, 159, 64, 1)'
+
+                  ],
+                  borderWidth: 1
+              }
+                  ]
+               },
+               options: {
+                   scales: {
+                       yAxes: [{
+                           ticks: {
+                               beginAtZero:true
+                           }
+                       }]
+                   },
+                   
+               }
+    
+           });
+    
+           this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+            
+                       type: 'line',
+                       data: {
+                           labels: ["Sesion Anterior", "Sesion Actual"],
+                           datasets: [
+                               {
+                                   label: "Peso",
+                                   fill: false,
+                                   lineTension: 0.1,
+                                   backgroundColor: "rgba(75,192,192,0.4)",
+                                   borderColor: "rgba(75,192,192,1)",
+                                   borderCapStyle: 'butt',
+                                   borderDash: [],
+                                   borderDashOffset: 0.0,
+                                   borderJoinStyle: 'miter',
+                                   pointBorderColor: "rgba(75,192,192,1)",
+                                   pointBackgroundColor: "#fff",
+                                   pointBorderWidth: 1,
+                                   pointHoverRadius: 5,
+                                   pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                                   pointHoverBorderColor: "rgba(220,220,220,1)",
+                                   pointHoverBorderWidth: 2,
+                                   pointRadius: 1,
+                                   pointHitRadius: 10,
+                                   data: [65, 59, 80],
+                                   spanGaps: false,
+                               }, {
+                                label: "Masa Corporal",
+                                fill: false,
+                                lineTension: 0.1,
+                                backgroundColor: "rgba(54, 162, 235, 0.2)",
+                                borderColor: "rgba(54, 162, 235, 1)",
+                                borderCapStyle: 'butt',
+                                borderDash: [],
+                                borderDashOffset: 0.0,
+                                borderJoinStyle: 'miter',
+                                pointBorderColor: "rgba(54, 162, 235, 1))",
+                                pointBackgroundColor: "#fff",
+                                pointBorderWidth: 1,
+                                pointHoverRadius: 5,
+                                pointHoverBackgroundColor: "rgba(54, 162, 235, 1)",
+                                pointHoverBorderColor: "rgba(220,220,220,1)",
+                                pointHoverBorderWidth: 2,
+                                pointRadius: 1,
+                                pointHitRadius: 10,
+                                data: [75, 69, 90],
+                                spanGaps: false,
+                            },
+                            {
+                              label: "% Muscular",
+                              fill: false,
+                              lineTension: 0.1,
+                              backgroundColor: "rgba(255, 206, 86, 0.2)",
+                              borderColor: "rgba(255,99,132,1)",
+                              borderCapStyle: 'butt',
+                              borderDash: [],
+                              borderDashOffset: 0.0,
+                              borderJoinStyle: 'miter',
+                              pointBorderColor: "rgba(255,99,132,1)",
+                              pointBackgroundColor: "#fff",
+                              pointBorderWidth: 1,
+                              pointHoverRadius: 5,
+                              pointHoverBackgroundColor: "rgba(255,99,132,1)",
+                              pointHoverBorderColor: "rgba(220,220,220,1)",
+                              pointHoverBorderWidth: 2,
+                              pointRadius: 1,
+                              pointHitRadius: 10,
+                              data: [45, 80, 50],
+                              spanGaps: false,
+                          }
+        
+                           ]
+                       }
+            
+                   });
+    
+       }
   
 }
