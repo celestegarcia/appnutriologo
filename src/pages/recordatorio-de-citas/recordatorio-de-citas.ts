57ba5f4ce39481a36744ac05b755a783fdb4ecfc
Yera <maauyerapa@ittepic.edu.mx>
2017-11-26 04:36:19 -0700

    Agregando boton fab de citas

--- src/pages/recordatorio-de-citas/recordatorio-de-citas.ts
+++ src/pages/recordatorio-de-citas/recordatorio-de-citas.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CitasPage } from '../citas/citas';
 
 @Component({
   selector: 'page-recordatorio-de-citas',
@@ -10,4 +11,8 @@ export class RecordatorioDeCitasPage {
   constructor(public navCtrl: NavController) {
   }
   
+  irCitas(params){
+    if (!params) params = {};
+    this.navCtrl.push(CitasPage);
+  }
 }
