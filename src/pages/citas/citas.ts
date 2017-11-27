7dcfbb94d49b2dd513ac6e3bd85bb4fbf14f8dc5
Luis Luna <ludolunapa@ittepic.edu.mx>
2017-11-26 23:13:48 -0700

    Agrega funcionalidad para crear citas

--- src/pages/citas/citas.ts
+++ src/pages/citas/citas.ts
@@ -5,17 +5,22 @@ import * as moment from 'moment';
 import { InicioPage } from '../inicio/inicio';
 
 import { TabsControllerPage } from '../tabs-controller/tabs-controller';
-
+//http
+import { Http, Headers, RequestOptions } from '@angular/http';
 @Component({
   selector: 'page-citas',
   templateUrl: 'citas.html'
 })
 export class CitasPage {
 
-  public hora:String =  "";
+  
   public fecha:String =  "";
-  public horas:string[] =["12:00","12:30","1:00","1:30"];
-
+  public horas:string[] =["9:00","9:30","10:00","10:30","11:00","11:30","12:00","12:30","1:00","1:30","2:00","2:30","3:00","3:30","4:00"];
+  public cita = {
+    hora:"",
+    fecha:"",
+    idpaciente:""
+  }
   eventSource = [];
   viewTitle: string;
   selectedDay = new Date();
@@ -24,7 +29,54 @@ export class CitasPage {
     mode: 'month',
     currentDate: new Date()
   };
-  constructor (public navCtrl: NavController, private modalCtrl: ModalController, private alertCtrl: AlertController) { }
+  constructor (public navCtrl: NavController,public http : Http , private modalCtrl: ModalController, private alertCtrl: AlertController ) 
+  { 
+      this.cita.idpaciente=(localStorage.getItem("paciente_id") as string);
+
+  }
+
+  obtenerHoras(){
+
+  }
+  confirmarPost(respuesta){
+    let alert = this.alertCtrl.create({
+      title: 'Registro exitoso',
+      subTitle: JSON.parse(respuesta).result,
+      buttons: ['Aceptar']
+    });
+    alert.present();
+    //this.navCtrl.push(LoginPage);
+    
+    this.navCtrl.setRoot(InicioPage);
+    this.navCtrl.popToRoot();
+  }
+prueba(){console.log(this.cita);}
+  prepararPost(){
+    //this.prereg.sexo= "F";
+    //this.prereg.fecha_naci="1991-01-01";
+    var body = this.cita;
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    //headers.append('access-control-allow-origin', '*');
+    console.log(this.cita);
+    //http://104.131.121.55/insertCita?fecha=01-01-2017&hora=10:30&paciente=1
+    this.http
+      .post('http://104.131.121.55/insertCita?paciente='+this.cita.idpaciente+
+            '&hora='+this.cita.hora+'&fecha='+this.cita.fecha,
+             null, { headers: headers }).toPromise()
+      .then(data => {
+        console.log((data as any)._body);
+        this.confirmarPost((data as any)._body);
+      }).catch(error => {
+        console.log(error);
+        let alert = this.alertCtrl.create({
+          title: 'Error al Registrar Cita',
+          subTitle: 'Hubo un error al procesar su solicitud, verifique su información, o intentelo más tarde.',
+          buttons: ['Regresar']
+        });
+        alert.present();
+      });
+  }
   goToConfirmarCita(params){
     if (!params) params = {};
     this.navCtrl.push(ConfirmarCitaPage);
