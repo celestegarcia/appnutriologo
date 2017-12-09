cec752cceb4e15abdaa22f5a6921a74466385c94
Yera <maauyerapa@ittepic.edu.mx>
2017-12-09 16:57:54 -0700

    Configuracion de avances con fotos

--- src/pages/album/album.ts
+++ src/pages/album/album.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 import { NavController, AlertController } from "ionic-angular";
 import { Camera, CameraOptions } from "@ionic-native/camera";
 import { AuthService } from "../../providers/auth-service/auth-service";
+import { Http, Headers, RequestOptions } from '@angular/http';
+
 // import {
 //   FileTransfer,
 //   FileUploadOptions,
@@ -13,7 +15,7 @@ import { AuthService } from "../../providers/auth-service/auth-service";
   templateUrl: "album.html"
 })
 export class AlbumPage {
-  public photos: any;
+  public photos: any = [];
   public base64Image: string;
   public fileImage: string;
   public responseData: any;
@@ -22,13 +24,33 @@ export class AlbumPage {
     public navCtrl: NavController,
     private camera: Camera,
     private alertCtrl: AlertController,
-    public authService: AuthService
+    public authService: AuthService,
+    public http : Http
     //private transfer: FileTransfer, private file: File, private fileUploadOptions: FileUploadOptions
   ) {}
   //const fileTransfer = this.transfer.create();
-  ngOnInit() {
-    this.photos = [];
-  }
+ 
+  ionViewDidLoad() {
+    
+        let headers: any = new Headers({'Content-Type': 'multipart/form-data'});
+        headers = new Headers({ 'Content-Type': 'application/json' });
+        //headers.append('access-control-allow-origin', '*');
+        let id = localStorage.getItem("paciente_id");
+        this.http.get('http://104.131.121.55/getPicturesSeg?id='+id).subscribe(res => {
+          var resultado = res.json().result;
+          this.photos = resultado;
+        }, error => {
+          let alert = this.alertCtrl.create({
+            title: 'Error al Obtener las fotos',
+            subTitle: 'Hubo un error al obtener sus fotos, intentelo más tarde.',
+            buttons: ['Regresar']
+          });
+          alert.present();
+        });
+    
+      }
+ 
+
 
   deletePhoto(index) {
     let confirm = this.alertCtrl.create({
@@ -53,44 +75,53 @@ export class AlbumPage {
     confirm.present();
   }
 
-  takePhoto() {
-    console.log("coming here");
+  takePic(){
+    this.camera.getPicture({
+     destinationType: this.camera.DestinationType.DATA_URL,
+     targetWidth: 1000,
+     targetHeight: 1000,
+    }).then((ImageData) => {
+      this.base64Image = 'data:image/jpeg;base64,' + ImageData;
 
-    const options: CameraOptions = {
-      quality: 50,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE,
-      targetWidth: 450,
-      targetHeight: 450,
-      saveToPhotoAlbum: false
-    };
+    }, (err) =>{
+      console.log(err);
+    });
+    }
 
-    this.camera.getPicture(options).then(
-      imageData => {
-        this.base64Image = "data:image/jpeg;base64," + imageData;
-        this.photos.push(this.base64Image);
-        this.photos.reverse();
-        this.sendData(imageData);
-      },
-      err => {
-        console.log(err);
-      }
-    );
-  }
+    enviarImg(){
+      let id = localStorage.getItem("paciente_id");
+      let body = {id:id,
+                 foto:this.base64Image}
+       let headers: any = new Headers({'Content-Type': 'multipart/form-data'});
+       headers = new Headers({ 'Content-Type': 'application/json' });
+       //headers.append('Content-Type', 'application/x-www-form-urlencoded');
+         this.http
+         .post('http://104.131.121.55/setPicturesSeg',
+               body, { headers: headers }).toPromise()
+         .then(data => {
+           console.log((data as any)._body);
+           this.confirmarPost((data as any)._body);
+         }).catch(error => {
+           console.log(error);
+           let alert = this.alertCtrl.create({
+             title: 'Error al Subir',
+             subTitle: 'Hubo un error al Actualizar el registro',
+             buttons: ['Aceptar']
+           });
+           alert.present();
+         });
+     
+   }
+    confirmarPost(respuesta){
+     let alert = this.alertCtrl.create({
+       title: 'Registro exitoso',
+       subTitle: JSON.parse(respuesta).result,
+       buttons: ['Aceptar']
+     });
+     alert.present();
+     //this.navCtrl.push(LoginPage);
+ 
+   }
 
-  sendData(imageData) {
-    this.userData.imageB64 = imageData;
-    this.userData.user_id = "1";
-    this.userData.token = "222";
-    console.log(this.userData);
-    this.authService.postData(this.userData, "userImage").then(
-      result => {
-        this.responseData = result;
-      },
-      err => {
-        // Error log
-      }
-    );
-  }
+  
 }
